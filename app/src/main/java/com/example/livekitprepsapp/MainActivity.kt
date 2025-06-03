4907cde498cd418e97db69a3b0920878f9783d36
Rahul bodh <rahulbodh819@gmail.com>
2025-06-03 11:28:22 +0530

    main activity for creadentials

--- app/src/main/java/com/example/livekitprepsapp/MainActivity.kt
+++ app/src/main/java/com/example/livekitprepsapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.livekitprepsapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.livekitprepsapp.databinding.ActivityMainBinding
+import io.livekit.android.sample.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,10 +27,74 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val urlString = viewModel.getSavedUrl()
+        val tokenString = viewModel.getSavedToken()
+        val e2EEOn = viewModel.getE2EEOptionsOn()
+        val e2EEKey = viewModel.getSavedE2EEKey()
+
+        binding.run {
+            url.editText?.text = SpannableStringBuilder(urlString)
+            token.editText?.text = SpannableStringBuilder(tokenString)
+            e2eeEnabled.isChecked = e2EEOn
+            e2eeKey.editText?.text = SpannableStringBuilder(e2EEKey)
+
+
+            connectButton.setOnClickListener {
+                val intent = Intent(this@MainActivity, CallActivity::class.java).apply {
+                    putExtra(
+                        CallActivity.KEY_ARGS,
+                        CallActivity.BundleArgs(
+                            url = url.editText?.text.toString(),
+                            token = token.editText?.text.toString(),
+                            e2eeOn = e2eeEnabled.isChecked,
+                            e2eeKey = e2eeKey.editText?.text.toString(),
+                            stressTest = StressTest.None,
+                        ),
+                    )
+                }
+
+                startActivity(intent)
+            }
+
+            saveButton.setOnClickListener {
+                viewModel.setSavedUrl(url.editText?.text?.toString() ?: "")
+                viewModel.setSavedToken(token.editText?.text?.toString() ?: "")
+                viewModel.setSavedE2EEOn(e2eeEnabled.isChecked)
+                viewModel.setSavedE2EEKey(e2eeKey.editText?.text?.toString() ?: "")
+
+                Toast.makeText(
+                    this@MainActivity,
+                    "Values saved.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+
+            resetButton.setOnClickListener {
+                viewModel.reset()
+                url.editText?.text = SpannableStringBuilder(MainViewModel.URL)
+                token.editText?.text = SpannableStringBuilder(MainViewModel.TOKEN)
+                e2eeEnabled.isChecked = false
+                e2eeKey.editText?.text = SpannableStringBuilder("")
+
+                Toast.makeText(
+                    this@MainActivity,
+                    "Values reset.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+        }
+
+        requestNeededPermissions()
 
 
 
 
 
+
+
+    }
+
+    private fun requestNeededPermissions() {
+        TODO("Not yet implemented")
     }
 }
