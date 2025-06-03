7a81ecc47f1f7ffae7f09a24b58d6ae488ec907a
Rahul bodh <rahulbodh819@gmail.com>
2025-06-03 12:40:19 +0530

    add drawables

--- app/src/main/java/com/example/livekitprepsapp/view/CallActivity.kt
+++ app/src/main/java/com/example/livekitprepsapp/view/CallActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.livekitprepsapp.R
+import com.example.livekitprepsapp.databinding.ActivityCallBinding
 
 class CallActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCallBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_call)
+        binding = ActivityCallBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
